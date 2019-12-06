18633c60614e85b3cacf2a7a4d9174a204a0fbea
Benjamin Shirley-Quirk <shirleyquirk[at]gm>
2019-12-06 14:47:17 +0000

    symlink .ssh/authorized_keys so ssh-copy-id works

--- install.sh
+++ install.sh
@@ -12,13 +12,21 @@ ssh -o ControlPath=None \
     -o IdentitiesOnly=yes $1 exit 2>/tmp/logfile.stderr </dev/null
 if [ $? != 0 ]; then
     echo "Copying publickey to openwrt router"
-    #make sure key exists
     ssh $1 "tee -a /etc/dropbear/authorized_keys" < ~/.ssh/id_rsa.pub
+    ssh $1 "test -e /root/.ssh"
+    if [ $? != 0 ]; then
+        ssh $1 "mkdir /root/.ssh"
+    fi
+    ssh $1 "test -e /root/.ssh/authorized_keys"
+    if [ $? != 0 ]; then
+        ssh $1 "cd /root/.ssh && ln -s /etc/dropbear/authorized_keys"
+    fi
 fi
 echo "installing dependencies"
 ssh $1 "opkg update"
 ssh $1 "opkg install libstdcpp"
 echo "Installing files to router"
+#TODO do an rsync test for age
 scp ./fadecandyserver $1:/etc/init.d/
 scp ./fcserver $1:/usr/bin/
 scp ./fcserver.config $1:/etc/
